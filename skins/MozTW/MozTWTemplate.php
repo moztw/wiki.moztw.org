b542d7aa57f38e3b8dd81e17069949695bccf317
Peter Pin-Guang Chen <petercpg@mail.moztw.org>
2015-06-05 13:13:31 +0800

    Update GA tracking code

--- skins/MozTW/MozTWTemplate.php
+++ skins/MozTW/MozTWTemplate.php
@@ -35,18 +35,12 @@ class MozTWTemplate extends QuickTemplate {
     <?php if($this->data['usercss'   ]) { ?><style type="text/css"><?php              $this->html('usercss'   ) ?></style><?php    } ?>
     <?php if($this->data['userjs'    ]) { ?><script type="text/javascript" src="<?php $this->text('userjs'    ) ?>"></script><?php } ?>
     <?php if($this->data['userjsprev']) { ?><script type="text/javascript"><?php      $this->html('userjsprev') ?></script><?php   } ?>
-	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
-</script>
-<script type="text/javascript">
-_uacct = "UA-1035080-1";
-urchinTracker();
-</script>
   </head>
 
   <body <?php if($this->data['body_ondblclick']) { ?>ondblclick="<?php $this->text('body_ondblclick') ?>"<?php } ?>
         <?php if($this->data['nsclass'        ]) { ?>class="secondLevel sectionWiki <?php      $this->text('nsclass')         ?>"<?php } ?>>
 <div id="header">
-<h1><a href="http:s//moztw.org/" title="Return to home page, moztw.org" accesskey="1">Mozilla Taiwan (MozTW)</a></h1>
+<h1><a href="https://moztw.org/" title="Return to home page, moztw.org" accesskey="1">Mozilla Taiwan (MozTW)</a></h1>
  <ul>
   <li id="menu_aboutus"><a href="https://moztw.org/about" title="About this website">關於本站</a></li>
   <li id="menu_contribute"><a title="了解我們正在做什麼，看看您能幫上什麼忙！" href="https://moztw.org/contribute">參與我們</a></li>
@@ -186,6 +180,15 @@ urchinTracker();
       </div></div>
     </div>
     <?php $this->html('reporttime') ?>
+    <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-1035080-1', 'auto');
+    ga('send', 'pageview');
+    </script>
   </body>
 </html>
 <?php
