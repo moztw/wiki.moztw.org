bbe231c5afbdbd19069a68ca6625d4e1a265d93c
Peter Pin-Guang Chen <petercpg@mail.moztw.org>
2015-10-17 12:46:49 +0800

    upgrade to mediawiki 1.25.3

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit9b10cc5cf6896d6e4a31983fc3498786::getLoader();
+return ComposerAutoloaderInite2ef576e4023c6f0949f5e8e1cdf8649::getLoader();
