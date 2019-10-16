098be43fca18464d4bff13c144030f3060486912
Peter Pin-Guang Chen <petercpg@mail.moztw.org>
2019-10-16 15:03:20 +0800

    Upgrade to mediawiki 1.32.3

--- includes/logging/LogEventsList.php
+++ includes/logging/LogEventsList.php
@@ -574,7 +574,7 @@ class LogEventsList extends ContextSource {
 	 * Determine if the current user is allowed to view a particular
 	 * field of this log row, if it's marked as restricted log type.
 	 *
-	 * @param stdClass $row
+	 * @param stdClass $type
 	 * @param User|null $user User to check, or null to use $wgUser
 	 * @return bool
 	 */
